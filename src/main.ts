a8470971ec909d9acb248a39004c3673175fc74b
William Guerrero Morgan <gmwill934@Williams-MacBook-Pro.local>
2021-01-17 14:51:08 -0600

    Added callback to listen method

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug'],
+  });
   app.enableCors();
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(3000, () =>
+    console.log(`Server running at http://localhost:3000`),
+  );
 }
 bootstrap();
